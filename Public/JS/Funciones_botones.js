a18a7b6801ac1903c4f4454de648c45c373270f0
Jesus <jesusdavidgonzales1102@gmail.com>
2022-09-23 21:02:23 -0500

    mejoras y nuevas funciones

--- Public/JS/Funciones_botones.js
+++ Public/JS/Funciones_botones.js
@@ -1,14 +1,21 @@
 const menuham = document.querySelector('#menu-ham')
 const link = document.querySelector('#link-menu')
 const ul = document.querySelector('#ul-menu')
+const cancel = document.querySelector('#icon-cancel')
 
 menu_mobile.addEventListener('click', (e)=>{
     menuham.classList.remove('remove')
     menuham.classList.add('menu')
-    console.log('click')
+    ul.classList.remove('remove')
+    ul.classList.add('lista-menu')
 })
 
 link.addEventListener('click',()=>{
+    menuham.classList.add('remove')
+    ul.classList.add('remove')
+})
+
+cancel.addEventListener('click', ()=>{
     menuham.classList.add('remove')
     ul.classList.add('remove')
 })
